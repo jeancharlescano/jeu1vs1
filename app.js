fdb11b0c50d502675dd8cf33af7ee0edb3e1f84d
Jean-Charles <jeancharles.cano@outlook.fr>
2021-03-25 12:55:20 +0100

    creation class player, + crea p1

---
+++ app.js
@@ -0,0 +1,19 @@
+var prompt = require("prompt");
+prompt.start();
+var readline = require('readline-sync');
+
+class Player {
+  constructor(name, life, xp, level) {
+    this.name = name;
+    this.life = life;
+    this.xp = xp;
+    this.level = level;
+  }
+}
+    name = readline.question("Quel est votre pseudo ? \n")
+    const player1 = new Player(name, 10, 100, 50);
+    console.log("🚀 ~ file: app.js ~ line 14 ~ player1", player1)
+
+
+
+
